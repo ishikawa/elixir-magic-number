e9a2db86baebb92638f9c2a5e16bce24cafb16b5
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2016-03-30 00:27:08 +0900

    Add tests

--- test/magic_number_test.exs
+++ test/magic_number_test.exs
@@ -2,7 +2,14 @@ defmodule MagicNumberTest do
   use ExUnit.Case
   doctest MagicNumber
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  # Path to fixtures directory which contains sample files.
+  @fixtures_dir Path.join(__DIR__, "fixtures")
+
+  test "MagicNumber.detect" do
+    content = @fixtures_dir
+              |> Path.join("000.gif")
+              |> File.read!
+
+    assert MagicNumber.detect(content) == {:ok, {:image, :gif}}
   end
 end
