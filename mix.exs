ce8582b4188d445c395c275a7a403041de98abeb
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2016-04-02 13:41:52 +0900

    Add documentation library

--- mix.exs
+++ mix.exs
@@ -29,7 +29,10 @@ defmodule MagicNumber.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:earmark, "~> 0.2.1", only: :dev},
+      {:ex_doc, "~> 0.11.4", only: :dev},
+    ]
   end
 
   defp package do
