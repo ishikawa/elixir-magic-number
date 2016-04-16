834d18fbfd85ab330d99ca462a8a7ec49b32a433
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2016-04-16 20:09:41 +0900

    Update docs confguration

--- mix.exs
+++ mix.exs
@@ -9,25 +9,15 @@ defmodule MagicNumber.Mixfile do
      start_permanent: Mix.env == :prod,
      description: "MagicNumber is the module to determine a file's type from its magic number.",
      package: package,
-     deps: deps]
+     deps: deps,
+     source_url: "https://github.com/ishikawa/elixir-magic-number",
+     docs: [extras: ["README.md"]]]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:earmark, "~> 0.2.1", only: :dev},
